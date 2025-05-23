d1623e40b9185a3150b2ea6443c6bb90ff158359
Santiago Lara <santiago.lara.galaz@gmail.com>
2025-05-23 00:38:39 -0400

    Refactor Header component to include a search input.
    Replaced placeholder content in the Header component with a functional search bar using Chakra UI components and a search icon from react-icons. This improves the UI and allows for future implementation of search functionality.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import {Container, Input, InputGroup, InputLeftElement} from "@chakra-ui/react";
+import {IoMdSearch} from "react-icons/io";
 
-type Props = {};
+function Header() {
+    return (
+        <Container maxW='3xl' mt="1" centerContent>
+            <InputGroup>
+                <InputLeftElement pointerEvents='none'>
+                    <IoMdSearch color="gray"/>
+                </InputLeftElement>
+                <Input type='text' placeholder='Try with Chicken or Beans'/>
+            </InputGroup>
+        </Container>
 
-function Header({}: Props) {
-  return <div>Header</div>;
+    )
 }
 
 export default Header;
